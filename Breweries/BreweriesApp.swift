c9a0ec550571758c90042f3347e9cb0f89fca3b4
Julia <juliakuntsevich@gmail.com>
2022-11-05 11:35:42 +0100

    Initial Commit

---
+++ Breweries/BreweriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BreweriesApp.swift
+//  Breweries
+//
+//  Created by Yuliya Ratkevcih on 05.11.22.
+//
+
+import SwiftUI
+
+@main
+struct BreweriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
